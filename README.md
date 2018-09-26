74d49723c1a1f813a4f560bb0975deabaaeaa8b7
Marcel Cruz <marcelcruz@live.com>
2018-09-26 09:53:58 +0100

    add reverse string

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # AlgoCasts
+
+Go inside exercises folder
+
+```
+cd exercises
+```
+
+Run tests for a specific exercise
+
+```
+jest fib --watch
+```
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// Solution 1
+// Turn into array, reverse it and turn into string
+
+// Solution 2
+// Create empty string and insert each character of str to the start of that string
+
+// Solution 3
+// Turn into array and use reduce
+
 function reverse(str) {}
 
-module.exports = reverse;
+reverse('abcd') // Needed for using the debugger
+
+module.exports = reverse
