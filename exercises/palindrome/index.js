b8d818d5ad49402032c7201f7cba6bdfff4cdc90
Marcel Cruz <marcelcruz@live.com>
2018-09-26 10:39:41 +0100

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// Solution 1
+// Use one of the solutions from reversestring and compare str to the reversed string
+
+// Solution 2
+// Turn into array and use every to compare the opposite characters
+// Not 100% efficient because it passes the middle of str.length when comparing
+
 function palindrome(str) {}
 
-module.exports = palindrome;
+module.exports = palindrome
