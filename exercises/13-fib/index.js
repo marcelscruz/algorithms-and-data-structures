1090423db05b808481ceddff33410e0652d1f5a0
Marcel Cruz <marcelcruz@live.com>
2018-10-04 10:34:23 +0100

    fibonacci

---
+++ exercises/13-fib/index.js
@@ -0,0 +1,59 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+// Solution 1 - Complexity: n
+// Create array with first two elements [0, 1]
+// Loop from the third element to n
+// - Create two variables, one for each previous number
+// - Push to the array the sum of these numbers
+// Return array at index n
+
+// Solution 2 - Recursive - Complexity: 2 ^ n, this is bad, fib(5) 15 func calls, fib(6) 25 func calls
+// If n < 2, return n
+// If not, return fib(n - 1) + fib(n - 2)
+// Check diagram
+// Also check tests time to understand complexity
+// ---------
+// Memoization
+// Create memoize function that receives a fn as argument
+// Create empty object 'cache'
+// Return a function that creates an array of the passed arguments - ...args
+// If 'cache' already has the set of args, return it from there
+// If not, create 'result' to store the result of passed function applying args
+// Store it in the 'cache'
+// Return 'result'
+// Make sure that 'fib' is a reference to the memoized function
+
+function memoize(fn) {
+  const cache = {}
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+
+    const result = fn.apply(this, args)
+    cache[args] = result
+
+    return result
+  }
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n
+  }
+
+  return fib(n - 1) + fib(n - 2)
+}
+
+fib = memoize(fib)
+
+module.exports = fib
